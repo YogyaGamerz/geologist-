4e7ab62d9ead9b0e0cf6f606e18d65410b3af8d1
YogyaGamerz <77446022+YogyaGamerz@users.noreply.github.com>
2021-04-20 14:47:48 +0530

    Add files via upload

--- Stone.js
+++ Stone.js
@@ -1,13 +1,13 @@
-class Iron {
-    constructor(x, y) {
+class Stone {
+    constructor(x, y, width, height) {
       var options = {
           'restitution':0.8,
-          'friction':3,
-          'density':30
+          'friction':0.9,
+          'density':12
       }
-      this.body = Bodies.rectangle(x, y, 50, 50, options);
-      this.width = 80;
-      this.height = 50;
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
       
       World.add(world, this.body);
     }
@@ -18,8 +18,9 @@ class Iron {
       translate(pos.x, pos.y);
       rotate(angle);
       rectMode(CENTER);
-
-      fill("brown");
+      strokeWeight(4);
+      stroke("black");
+      fill("black");
       rect(0, 0, this.width, this.height);
       pop();
     }
